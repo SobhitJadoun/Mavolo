4863d0f8509f9dfd00cb19cdf88c3804c0a766dc
Sobhit Jadoun <62428157+SobhitJadoun@users.noreply.github.com>
2024-03-11 18:26:24 +0530

    changes

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -13,18 +13,12 @@ export default function Footer() {
                 <i class="fa-brands fa-instagram"></i></div>
             </div>
         </div>
-        <div className="row ">
-          <div className="col-12 col-sm-6 col-md-6 add">
+        <div className="row add">
+          <div className="col-12 col-sm-6 col-md-6">
             <h4 className='font-weight-bold'>Register Address</h4>
             <p>Block - C -168,Second floor, sector 10,Noida Gautam Buddha Nagar ,India - 201301</p>
-            <p>For any queries or concerns, write to us at           <b>preinfo@mavolo.in</b></p>
           </div>
-          <div className="col-12 col-sm-6 col-md-6">
-            <div className="right">
-            <i class="fa-solid fa-phone"></i> 
-            <span> Mob- +91 8861587589, 7892653145, 9787093848</span>
-            </div>
-          </div>
+          <div className="col-12 col-sm-6 col-md-6"></div>
         </div>
       </div>
     </> 
